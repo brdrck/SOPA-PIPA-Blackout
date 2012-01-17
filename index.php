248fae4bbf2d64793116f88f92cdb1d26f1e3dc5
Jeff Broderick <jeff@brdrck.me>
2012-01-17 15:36:39 -0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+
+<html id="<?=$htmlID;?>" class="<?=($status !== 0)?'iphone':'desktop';?>">
+<head>
+
+	<title>Stop American Censorship!</title>
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />  
+	<meta name="viewport" content="user-scalable=no; width=device-width; initial-scale=1.0; maximum-scale=1.0;"> 
+	<link rel="shortcut icon" href="_/images/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" href="_/images/ios-icon.png">
+	<link rel="apple-touch-startup-image" href="_/images/ios-splash.jpg" />  
+	
+	<link rel="stylesheet" href="_/styles/main.css" type="text/css">
+	
+	<script src="_/scripts/jquery.min.js"></script>
+	<script>
+	    window.onload = function() {
+	        setTimeout(function() { window.scrollTo(0, 1) }, 100);
+	    }
+	</script>
+</head>
+<body>
+	<div id="container">
+		<div id="logo"></div>
+		<h1>Congress is considering a law<br />
+		That would <b>censor the web</b></h1>
+		<p>Sites you use every day<br />
+		<b>could be blocked</b> if it passes</p>
+		<a href="http://americancensorship.org">Take action now!</a>
+	</div>
+
+	
+	<script type="text/javascript">
+		var _gauges = _gauges || [];
+		(function() {
+		var t   = document.createElement('script');
+		t.type  = 'text/javascript';
+		t.async = true;
+		t.id    = 'gauges-tracker';
+		t.setAttribute('data-site-id', '4f15f64acb25bc723500000a');
+		t.src = '//secure.gaug.es/track.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(t, s);
+		})();
+	</script>
+</body>
+</html>
