9e9e7ca7ea26fc34091f5add5c164e26d0d2e9a7
Jeff Broderick <jeff@brdrck.me>
2012-01-17 17:52:48 -0800

    Added a more information link and copy.
    I'm not much of a writer, so if you see any typos or mistakes please
    let me know or correct them.

--- index.php
+++ index.php
@@ -33,10 +33,29 @@ header('Status: 503 Service Temporarily Unavailable');
 	<div id="container">
 		<div id="logo"></div>
 		<h1>Congress is considering a law<br />
-		That would <b>censor the web</b></h1>
+		That would <strong>censor the web</strong></h1>
 		<p>Sites you use every day<br />
-		<b>could be blocked</b> if it passes</p>
+		<strong>could be blocked</strong> if it passes</p>
 		<a href="http://americancensorship.org">Take action now!</a>
+		<a class="learnmore" href="#learnmore" onclick="javascript:$('html').addClass('article');$('a.learnmore').hide();$('#learnmore').show();">Learn more</a>
+		
+		<a class="nostyle" name="learnmore"></a>
+		<div id="learnmore" style="display: none;">
+			<h1>What is SOPA?</h1>
+			<p>Also known as the Stop Online Piracy Act, SOPA is a proposed bill, within the House, which aims to crack down on copyright infringement by restricting access to sites that host pirated content.</p>
+			<p>The bill could require your Internet provider to block access to sites suspected of containing or linking to copyrighted content.  As well as force search engines like Google, Yahoo and Bing to stop linking to them.  And payment processors like Paypal to couldn't transfer funds to them.</p>
+			<p>If SOPA were to become law, the Internet in America would begin to appear similar to the Internet in countries like China. China has what is basically a government-controlled firewall in place, blocking access to sites for any number of reasons. Essentially sites that you'd want to visit, sites that you go to on a daily basis could be shutdown in the blink of an eye, with little-to-no judicial due-process.</p>
+			
+			<h1>What does this mean for me?</h1>
+			<p>Well, currently, sites like YouTube hold the uploader liable for the content they add.  With SOPA, YouTube is liable. This will force sites like YouTube to <strong>censor their content</strong> in order to prevent any legal actions.  This can also be said for sites like Facebook, Reddit, MySpace, Tumblr, Wikipedia, and any other user curated content sites.</p>
+			
+			<h1>Does this effect non-US citizens?</h1>
+			<p>Yes, some of the most popular websites today are located in America. <strong>So you will be censored too.</strong></p>
+			
+			<h1>What can I do to prevent this?</h1>
+			<p>You can help by <a target="_blank" href="http://americancensorship.org">writing your senator</a> and tell them you oppose SOPA & PIPA.</p>
+			<p>If you own a website, you can spread awareness by displaying a page similar to this or redirecting it to <a target="_blank" href="http://brdrck.me/sopa">http://brdrck.me/sopa</a>.</p>
+		</div>
 	</div>
 </body>
 </html>
